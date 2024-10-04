3fe4df37c7673ed43e950b0e40f9d47f9ef133e2
Sneha Tiwari <snehhhcodes@gmail.com>
2024-10-04 21:32:42 +0530

    botchanges

--- assets/js/botpress.js
+++ assets/js/botpress.js
@@ -4,12 +4,11 @@ document
   .getElementById("chatWithArthsathi")
   .addEventListener("click", function () {
     var script1 = document.createElement("script");
-    script1.src = "https://cdn.botpress.cloud/webchat/v1/inject.js";
+    script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
     document.head.appendChild(script1);
 
     var script2 = document.createElement("script");
-    script2.src =
-      "https://mediafiles.botpress.cloud/5e160793-5606-4412-ba4c-337e416f14fe/webchat/config.js";
+    script2.src = "https://files.bpcontent.cloud/2024/10/03/17/20241003171827-927X92KD.js";
     script2.defer = true;
     document.head.appendChild(script2);
   });
